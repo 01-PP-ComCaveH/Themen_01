3aa4c9882edbb5960e95209de22d661acd9bd473
micontosh <168723056+micontosh@users.noreply.github.com>
2024-05-08 10:46:53 +0000

    var + prompt

---
+++ src/02_variablen.js
@@ -0,0 +1,34 @@
+// Zeilenkommentar | Strg + #
+
+/**Mehrzeiliger
+ * Kommentar
+ */
+
+
+
+/***** Variablen 01*****/
+
+// console.log("hi");
+// var hi=(3+2);
+// console.log(hi);
+
+/***** Deklaration und Zuwesung******/
+
+// let firstName;
+// firstName = "Deinname";
+// console.log(firstName);
+// let familyName = "Istvergeben";
+// console.log(familyName);
+
+// console.log("Du heisst wirklich " + firstName + " " + familyName + "?");
+
+// let test;
+// test = (3+"lol")
+// console.log("Inhalt:" + test);
+// console.log("Datentyp:" + typeof test);
+
+/******** Konstanten ************/
+const test2 = 6;
+test2 = 5;
+console.log("Inhalt:" + test2);
+console.log("Datentyp:" + typeof test2);
